a9788ffab28006ec60883affbc26f7a69dfc0bee
Aviad Rozenhek <aviadr1@gmail.com>
2024-10-17 22:30:11 +0300

    Update ws_server.py

--- ws_server.py
+++ ws_server.py
@@ -30,17 +30,10 @@ from libs.whisper_streaming.whisper_online import (  # add_shared_args,
     load_audio_chunk,
 )
 
-# from libs.whisper_streaming.whisper_online_server import online
-
-
 def my_set_logging(args, logger, other="_server"):
     logging.basicConfig(format="%(levelname)s\t%(message)s")  # format='%(name)s
     logger.setLevel(args.log_level)
     logging.getLogger("whisper_online" + other).setLevel(args.log_level)
-
-
-#    logging.getLogger("whisper_online_server").setLevel(args.log_level)
-
 
 logging.basicConfig(format="%(levelname)s\t%(message)s", level=logging.INFO)  # format='%(name)s
 logger = logging.getLogger(__name__)
@@ -54,25 +47,6 @@ AUDIO_FILE_URL = "https://raw.githubusercontent.com/AshDavid12/runpod-serverless
 
 app = FastAPI()
 args = argparse.ArgumentParser()
-
-# def my_add_shared_args(parser):
-#     """shared args for simulation (this entry point) and server
-#     parser: argparse.ArgumentParser object
-#     """
-#     parser.add_argument('--min-chunk-size', type=float, default=1.0, help='Minimum audio chunk size in seconds. It waits up to this time to do processing. If the processing takes shorter time, it waits, otherwise it processes the whole segment that was received by this time.')
-#     # AVIAD: allow custom model
-#     parser.add_argument('--model', type=str, help="Name size of the Whisper model to use. The model is automatically downloaded from the model hub if not present in model cache dir.")
-#     parser.add_argument('--model_cache_dir', type=str, default=None, help="Overriding the default model cache dir where models downloaded from the hub are saved")
-#     parser.add_argument('--model_dir', type=str, default=None, help="Dir where Whisper model.bin and other files are saved. This option overrides --model and --model_cache_dir parameter.")
-#     parser.add_argument('--lan', '--language', type=str, default='auto', help="Source language code, e.g. en,de,cs, or 'auto' for language detection.")
-#     parser.add_argument('--task', type=str, default='transcribe', choices=["transcribe","translate"],help="Transcribe or translate.")
-#     parser.add_argument('--backend', type=str, default="faster-whisper", choices=["faster-whisper", "whisper_timestamped", "openai-api"],help='Load only this backend for Whisper processing.')
-#     parser.add_argument('--vac', action="store_true", default=False, help='Use VAC = voice activity controller. Recommended. Requires torch.')
-#     parser.add_argument('--vac-chunk-size', type=float, default=0.04, help='VAC sample size in seconds.')
-#     parser.add_argument('--vad', action="store_true", default=False, help='Use VAD = voice activity detection, with the default parameters.')
-#     parser.add_argument('--buffer_trimming', type=str, default="segment", choices=["sentence", "segment"],help='Buffer trimming strategy -- trim completed sentences marked with punctuation mark and detected by sentence segmenter, or the completed segments returned by Whisper. Sentence segmenter must be installed for "sentence" option.')
-#     parser.add_argument('--buffer_trimming_sec', type=float, default=15, help='Buffer trimming length threshold in seconds. If buffer length is longer, trimming sentence/segment is triggered.')
-#     parser.add_argument("-l", "--log-level", dest="log_level", choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'], help="Set the log level", default='DEBUG')
 
 
 def check_fp16_support():
@@ -194,7 +168,7 @@ def convert_to_mono_16k(input_wav: str, output_wav: str) -> None:
     resampled_audio = librosa.resample(audio_data, orig_sr=original_sr, target_sr=SAMPLING_RATE)
 
     # Step 4: Save the resampled audio as a .wav file in mono at 16 kHz
-    sf.write(output_wav, resampled_audio, SAMPLING_RATE)
+    soundfile.write(output_wav, resampled_audio, SAMPLING_RATE)
 
     logger.info(f"Converted audio saved to {output_wav}")
 
